928595d0f37866d2ea6e2569c9f8f3dcd736bba3
VatoQv <v.qvaraia.j@gmail.com>
2022-11-28 19:59:09 +0400

    front-end changes to support functionality of adding new products

--- chapter 2. service mocking/static/app.js
+++ chapter 2. service mocking/static/app.js
@@ -6,10 +6,14 @@ server from ../service.js should also be running to fetch mock data
 
 const API = 'http://localhost:3000';
 
-const populateProducts = async (category) => {
+const populateProducts = async (category, method = 'GET', payload) => {
   const products = document.querySelector('#products');
   products.innerHTML = '';
-  const res = await fetch(`${API}/${category}`);
+  const send = method === 'GET' ? {} : {
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(payload)
+  }
+  const res = await fetch(`${API}/${category}`, { method, ...send });
   const data = await res.json();
   for (const product of data) {
     const item = document.createElement('product-item');
@@ -22,11 +26,26 @@ const populateProducts = async (category) => {
     products.appendChild(item);
   }
 }
+
 const category = document.querySelector('#category');
+const add = document.querySelector('#add');
 
 category.addEventListener('input', async ({ target }) => {
+  add.style.display = 'block';
   await populateProducts(target.value);
-})
+});
+
+add.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const { target } = e;
+  const payload = {
+    name: target.name.value,
+    rrp: target.rrp.value,
+    info: target.info.value
+  }
+  await populateProducts(category.value, 'POST', payload);
+  target.reset();
+});
 
 customElements.define('product-item', class Item extends HTMLElement {
   constructor() {
@@ -34,4 +53,4 @@ customElements.define('product-item', class Item extends HTMLElement {
     const itemTmpl = document.querySelector('#item').content;
     this.attachShadow({mode: 'open'}).appendChild(itemTmpl.cloneNode(true));
   }
-})
+});
